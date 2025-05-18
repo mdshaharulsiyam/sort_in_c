6ac7c8040ed2ef7824712d2a13e18916c9e67c70
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2025-05-18 16:45:26 +0600

    insertion sort aded

---
+++ insertion_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 6, 4, 1, 8, 3, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+ for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;  
+    }
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
